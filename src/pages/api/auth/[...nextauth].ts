5d97acf811a8a66ada43b68b521f187a98abf89d
NicholasWM <nicholaswesleymazzei@gmail.com>
2021-09-29 19:28:45 -0300

    add: Posts completed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,51 @@
 import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import providers from "next-auth/providers"
 import {fauna} from '../../../services/fauna'
+
 export default NextAuth({
   providers: [
-    GithubProvider({
+    providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope:'read:user'
     }),
   ],
   callbacks:{
+    async session(session){
+        try {
+            const userActiveSubscription = await fauna.query(
+                q.Get(
+                    q.Intersection([
+                        q.Match(
+                            q.Index('subscription_by_user_ref'),
+                            q.Select(
+                                'ref',
+                                q.Get(
+                                    q.Match(
+                                        q.Index('user_by_email'),
+                                        q.Casefold(session.user.email)
+                                    )
+                                )
+                            )
+                        ),
+                        q.Match(
+                            q.Index('subscription_by_status'),
+                            'active'
+                        )
+                    ])
+                )
+            )
+        return {...session, activeSubscription: userActiveSubscription}
+
+        } catch (error) {
+            return {
+                ...session,
+                activeSubscription:null
+            }            
+        }
+
+    },
     async signIn(user ) {
         const {email} = user
         console.log(email);
